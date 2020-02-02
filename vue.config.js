749bd31367d01d2ddd7c7dbadfaee995c6819933
Sanctuary <sanc.pw@gmail.com>
2020-02-02 11:40:08 -0500

    Add pwa options to vue config file

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
 	lintOnSave: false,
+	pwa: {
+		themeColor: '#4DBA87',
+		msTileColor: '#000000',
+		appleMobileWebAppCapable: 'yes',
+		appleMobileWebAppStatusBarStyle: 'black',
+	},
 	pluginOptions: {
 		autoRouting: {
 			chunkNamePrefix: 'page-',
